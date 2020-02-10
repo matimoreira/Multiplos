478e0aa9a57720ab691d9a1f4af918d3a0f54952
matimoreira <58670479+matimoreira@users.noreply.github.com>
2020-02-10 03:50:20 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,20 +8,20 @@ namespace Multiplos
         static void Main(string[] args)
         {
             int cantidadMultiplos;
-            //var multiplos = new List<int>();
+            var multiplos = new List<int>();
             //Console.WriteLine("Ingresar un numero cualquiera: ");
             //numero = int.Parse(Console.ReadLine());
             Console.Write("Ingresa la cantidad de multiplos de 3 y no de 5 que quieres obtener: ");
             cantidadMultiplos = int.Parse(Console.ReadLine());
             Console.WriteLine("Los primeros {0} multiplos de 3 y no de 5 que quieres obtener: ", cantidadMultiplos);
-            for (int i = 1; i <= cantidadMultiplos; i++)
+            while (multiplos.Count <= cantidadMultiplos)
             {
                 if (i % 3 == 0  &&  i % 5 != 0)
                 {
-                    Console.WriteLine(i);
+                    multiplos.add(i);
                 }
             }
-
+            Console.writeline(multiplos.ToString());
             Console.ReadKey();
         }
     }
